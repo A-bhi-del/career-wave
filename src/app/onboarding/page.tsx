9904cd4f92648c1c2e254a1bd7fed72c9754e4be
Abhishek Gupta <guptaabhishek60420@gmail.com>
2025-04-08 09:52:47 +0530

    sixth commit

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,28 +3,30 @@ import { prisma } from "../utils/db";
 import { redirect } from "next/navigation";
 import { requireUser } from "../utils/requireuser";
 
-export async function CheckIfOnboardingIscompleted(userId: string){
-    const user = await prisma.user.findUnique({
-        where: {
-            id: userId,
-        },
-        select: {
-            onboardingCompleted: true,
-        }
+async function checkIfOnboardingIscompleted(userId: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      onboardingCompleted: true,
+    },
+  });
 
-    });
-    if(user?.onboardingCompleted === true){
-        return redirect("/");
-    }
-    return user;
+  if (user?.onboardingCompleted === true) {
+    return redirect("/");
+  }
+
+  return user;
 }
-export default async function onboardingPage(){
-    const session = await requireUser();
-    await CheckIfOnboardingIscompleted(session.id as string);
 
-    return (
-        <div className="min-h-screen w-screen flex flex-col items-center justify-center py-10 ">
-            <Onboardingform/>
-        </div>
-    )
+export default async function onboardingPage() {
+  const session = await requireUser();
+  await checkIfOnboardingIscompleted(session.id as string);
+
+  return (
+    <div className="min-h-screen w-screen flex flex-col items-center justify-center py-10 ">
+      <Onboardingform />
+    </div>
+  );
 }
